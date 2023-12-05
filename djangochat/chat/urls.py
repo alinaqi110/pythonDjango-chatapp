f4a19a0e9821311f7567b384cd519532efa40699
Ali Naqi Shah <alinaqisayed74@gmail.com>
2023-12-05 11:01:11 +0500

    Add files via upload
    Complete project

---
+++ djangochat/chat/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('<str:room>/', views.room),
+    path('checkview', views.checkview),
+    path('send', views.send),
+    path('getMessages/<str:room>/', views.getMessages),
+
+]
